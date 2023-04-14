0894a1202c4f37ce2a9dc311dd9d74e496f0635b
Hajra <hajrabuksh@gmail.com>
2023-04-14 00:35:50 -0700

    completed successful

--- src/App.js
+++ src/App.js
@@ -16,18 +16,18 @@ export default class App extends Component{
     temp_min:undefined,
     icon:undefined,
     error:false,
-    description:undefined,
+    description:"",
     celcius:undefined,
   };
 
   this.weatherIcon={
-    Thunderstorm:"wi-thunderstorm",
-    Drizzle:"wi-sleet",
-    Rain:"wi-storm-showers",
-    Snow:"wi-snow",
-    Atmosphere:"wi-fog",
-    Clear:"wi-day-sunny",
-    Clouds:"wi-day-clouds",
+    Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog"
 
   }
   
@@ -39,30 +39,30 @@ export default class App extends Component{
     return cell;
   }
   get_weatherIcon(icons,rangeId){
-    switch(rangeId){
-      case rangeId >=200 && rangeId <=232:
-        this.setState({icon:icons.Thunderstorm})
-        break
-        case rangeId >=300 && rangeId <=331:
-          this.setState({icon:icons.Drizzle})
-          break
-          case rangeId >=500 && rangeId <=531:
-            this.setState({icon:icons.Rain})
-            break
-            case rangeId >=600 && rangeId <=622:
-            this.setState({icon:icons.Snow})
-            break
-            case rangeId >=701 && rangeId <=781:
-              this.setState({icon:icons.Atmosphere})
-              break
-              case rangeId === 800:
-              this.setState({icon:icons.Clear})
-              break
-              case rangeId >=801 && rangeId <=804:
-                this.setState({icon:icons.Clouds})
-                break
-                default:
-                this.setState({icon:icons.Clouds})
+    switch (true) {
+      case rangeId >= 200 && rangeId < 232:
+        this.setState({ icon: icons.Thunderstorm });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: icons.Drizzle });
+        break;
+      case rangeId >= 500 && rangeId <= 521:
+        this.setState({ icon: icons.Rain });
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: icons.Snow });
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: icons.Atmosphere });
+        break;
+      case rangeId === 800:
+        this.setState({ icon: icons.Clear });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: icons.Clouds });
+        break;
+      default:
+        this.setState({ icon: icons.Clouds });
     }
   }
   async getWeather(e){
@@ -88,16 +88,18 @@ export default class App extends Component{
         description: response.weather[0].description,
         error: false
       });
+
+      this.get_weatherIcon(this.weatherIcon,response.weather[0].id)
+      
     } catch (error) {
       console.error(error);
       this.setState({ error: true });
     }
 
-    this.get_weatherIcon(this.weatherIcon,this.response.weather[0].id)
   }
 
   render() {
-    console.log(this.state.city,"city");
+    console.log(this.state.icon,"iconssss");
     return (
       <div className="App">
 
